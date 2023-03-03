ef30e03e0959cf4bcf40525476cc2e3f7834c9bb
dwikiop <dwikiokvianp@gmail.com>
2023-03-04 03:14:51 +0700

    refactor - use error handler to handle all error received on user and city services

--- controllers/user-service/userController.js
+++ controllers/user-service/userController.js
@@ -1,44 +1,43 @@
-'use strict';
-const userApi = process.env.USER_API || 'http://localhost:4001';
+"use strict";
+const userApi = process.env.USER_API || "http://localhost:4001";
 
-const { default: axios } = require('axios');
+const { default: axios } = require("axios");
 
 module.exports = class UserController {
-    static async findAll(req, res) {
-        try {
-            const { data } = await axios.get(userApi + '/users');
-            res.status(200).json(data);
-        } catch (err) {
-            const { status, data } = err.response;
-            res.status(status).json(data);
-        }
+  static async findAll(req, res, next) {
+    try {
+      const { data } = await axios.get(userApi + "/users");
+      res.status(200).json(data);
+    } catch (err) {
+      next(err);
     }
-    static async findById(req, res) {
-        try {
-            const { id } = req.params;
-            const { data } = await axios.get(userApi + '/users/' + id);
-            res.status(200).json(data);
-        } catch (err) {
-            const { status, data } = err.response;
-            res.status(status).json(data);
-        }
+  }
+
+  static async findById(req, res, next) {
+    try {
+      const { id } = req.params;
+      const { data } = await axios.get(userApi + "/users/" + id);
+      res.status(200).json(data);
+    } catch (err) {
+      next(err);
     }
-    static async login(req, res) {
-        try {
-            const { data } = await axios.post(userApi + '/login', req.body);
-            res.status(200).json(data);
-        } catch (err) {
-            const { status, data } = err.response;
-            res.status(status).json(data);
-        }
+  }
+
+  static async login(req, res, next) {
+    try {
+      const { data } = await axios.post(userApi + "/login", req.body);
+      res.status(200).json(data);
+    } catch (err) {
+      next(err);
     }
-    static async register(req, res) {
-        try {
-            const { data } = await axios.post(userApi + '/register', req.body);
-            res.status(200).json(data);
-        } catch (err) {
-            const { status, data } = err.response;
-            res.status(status).json(data);
-        }
+  }
+
+  static async register(req, res, next) {
+    try {
+      const { data } = await axios.post(userApi + "/register", req.body);
+      res.status(200).json(data);
+    } catch (err) {
+      next(err);
     }
+  }
 };
