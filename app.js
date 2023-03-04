9994ef76de0bd8158b21b65545b59f856f88a850
dwikiop <dwikiokvianp@gmail.com>
2023-03-04 21:38:40 +0700

    feat - add filter city by province

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
 const port = process.env.PORT || 4000;
 
 //import routes
-const router = require('./routes/index');
-const errorHandler = require('./middlewares/errorHandler');
+const router = require("./routes/index");
+const errorHandler = require("./middlewares/errorHandler");
 
 //set up middleware
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,13 +21,13 @@ app.use(router);
 app.use(errorHandler);
 
 //set up socket io
-io.on('connection', function (socket) {
-    console.log('a user connected');
-    socket.on('disconnect', function () {
-        console.log('user disconnected');
-    });
+io.on("connection", function (socket) {
+  console.log("a user connected");
+  socket.on("disconnect", function () {
+    console.log("user disconnected");
+  });
 });
 
 server.listen(port, function () {
-    console.log('Server started on port ' + port);
+  console.log("Server started on port " + port);
 });
