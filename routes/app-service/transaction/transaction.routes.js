a80a8ac67a17408fd05d7a47504e2d57969ef53a
dwikiop <dwikiokvianp@gmail.com>
2023-03-05 14:09:20 +0700

    feat - orchestrated transaction endpoint and add get all transaction

--- routes/app-service/transaction/transaction.routes.js
+++ routes/app-service/transaction/transaction.routes.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const transactionController = require("../../../controllers/app-service/transaction/transaction.controller");
 
 router.post("/", transactionController.addTransaction);
+router.get("/", transactionController.getAllTransaction);
 router.get("/:id", transactionController.getTransaction);
 router.put("/:id", transactionController.updateTransaction);
 
