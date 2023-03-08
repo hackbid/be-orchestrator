320d934f76a0713a09b06d58c109f0198ff8aca4
Mitra Surya <MITRASURYA7@GMAIL.COM>
2023-03-08 11:50:14 +0700

    Update itemControllers.js

--- controllers/app-service/item/itemControllers.js
+++ controllers/app-service/item/itemControllers.js
@@ -1,90 +1,101 @@
-
-const { Upload } = require('@aws-sdk/lib-storage');
-const axios = require('axios');
-const redis = require('../../../config/redis.config');
-const { s3 } = require('../../../config/S3');
-const { invalidateCache } = require('../../../helper/invalidate.redis');
-
+const { Upload } = require("@aws-sdk/lib-storage");
+const axios = require("axios");
+const redis = require("../../../config/redis.config");
+const { s3 } = require("../../../config/S3");
+const { invalidateCache } = require("../../../helper/invalidate.redis");
 const itemAPI = process.env.ITEM_API;
 const mongoAPI = process.env.MONGO_API;
 const userAPI = process.env.USER_API;
 const categoryAPI = process.env.CATEGORY_API;
 const itemCache = {
-    items: 'items/all',
+  items: "items/all",
 };
 
 module.exports = class ItemController {
-    static async findAll(req, res, next) {
-        try {
-            const cachedData = await redis.get(itemCache.items);
-            if (cachedData) {
-                return res.status(200).json(JSON.parse(cachedData));
-            } else {
-                const { data } = await axios.get(itemAPI);
-                await redis.set(itemCache.items, JSON.stringify(data));
-                res.status(200).json(data);
-            }
-        } catch (err) {
-            next(err);
-        }
+  static async findAll(req, res, next) {
+    try {
+      const cachedData = await redis.get(itemCache.items);
+      if (cachedData) {
+        return res.status(200).json(JSON.parse(cachedData));
+      } else {
+        const { data } = await axios.get(itemAPI);
+        await redis.set(itemCache.items, JSON.stringify(data));
+        res.status(200).json(data);
+      }
+    } catch (err) {
+      next(err);
     }
-    static async findItemToday(req, res, next) {
-        try {
-            const { data: itemToday } = await axios.get(`${itemAPI}/today`);
-            const temp = itemToday.map(async (e) => {
-                const { data: imagesData } = await axios.get(mongoAPI + `/itemImages/${e.imageMongoId}`);
-                e.images = imagesData ? imagesData.images : [];
-                return e;
-            });
-            const result = await Promise.all(temp);
-            res.status(200).json(result);
-        } catch (err) {
-            next(err);
-        }
+  }
+  static async findItemToday(req, res, next) {
+    try {
+      const { data: itemToday } = await axios.get(`${itemAPI}/today`);
+      const temp = itemToday.map(async (e) => {
+        const { data: imagesData } = await axios.get(
+          mongoAPI + `/itemImages/${e.imageMongoId}`
+        );
+        e.images = imagesData ? imagesData.images : [];
+        return e;
+      });
+      const result = await Promise.all(temp);
+      res.status(200).json(result);
+    } catch (err) {
+      next(err);
     }
-    static async createItem(req, res, next) {
-        try {
-            const { name, SubCategoryId, description, startPrice, multiple, startDate, startHour, endHour, weight, UserId } = req.body;
-            const { data } = await axios.post(itemAPI, {
-                name,
-                SubCategoryId,
-                description,
-                startPrice,
-                multiple,
-                startDate,
-                startHour,
-                endHour,
-                status: 'posted',
-                weight,
-                UserId,
-            });
-            const imagesUrl = req.files.map(async (file) => {
-                const params = {
-                    Bucket: 'data-hackbid-app',
-                    Key: Math.floor(Math.random() * 1000) + '_' + `${name}_assets`,
-                    Body: file.buffer,
-                };
-                const uploadParallel = new Upload({
-                    client: s3,
-                    queueSize: 4, // optional concurrency configuration
-                    partSize: 5542880, // optional size of each part
-                    leavePartsOnError: false, // optional manually handle dropped parts
-                    params,
-                });
-                const { Location } = await uploadParallel.done();
-                return Location;
-            });
-            const images = await Promise.all(imagesUrl);
-            await axios.post(`${mongoAPI}/itemImages/${data.imageMongoId}`, {
-                images,
-            });
-            invalidateCache(itemCache.items);
-            res.status(200).json(data);
-        } catch (err) {
-            // next(err);
-            console.log(err);
-        }
+  }
+  static async createItem(req, res, next) {
+    try {
+      const {
+        name,
+        SubCategoryId,
+        description,
+        startPrice,
+        multiple,
+        startDate,
+        startHour,
+        endHour,
+        weight,
+        UserId,
+      } = req.body;
+      const { data } = await axios.post(itemAPI, {
+        name,
+        SubCategoryId,
+        description,
+        startPrice,
+        multiple,
+        startDate,
+        startHour,
+        endHour,
+        status: "posted",
+        weight,
+        UserId,
+      });
+      const imagesUrl = req.files.map(async (file) => {
+        const params = {
+          Bucket: "data-hackbid-app",
+          Key: Math.floor(Math.random() * 1000) + "_" + `${name}_assets`,
+          Body: file.buffer,
+        };
+        const uploadParallel = new Upload({
+          client: s3,
+          queueSize: 4, // optional concurrency configuration
+          partSize: 5542880, // optional size of each part
+          leavePartsOnError: false, // optional manually handle dropped parts
+          params,
+        });
+        const { Location } = await uploadParallel.done();
+        return Location;
+      });
+      const images = await Promise.all(imagesUrl);
+      await axios.post(`${mongoAPI}/itemImages/${data.imageMongoId}`, {
+        images,
+      });
+      invalidateCache(itemCache.items);
+      res.status(200).json(data);
+    } catch (err) {
+      // next(err);
+      console.log(err);
     }
+  }
 
   static async getItemById(req, res, next) {
     try {
@@ -122,24 +133,22 @@ module.exports = class ItemController {
       res.status(200).json(data);
     } catch (err) {
       next(err);
-
     }
-
-    static async updateItem(req, res, next) {
-        try {
-            const { UserId, amountBid } = req.body;
-            const { id } = req.params;
-            const { data } = await axios.put(`${itemAPI}/${id}`, {
-                UserId,
-                amountBid,
-            });
-            invalidateCache(itemCache.items);
-            res.status(200).json(data);
-        } catch (err) {
-            next(err);
-        }
+  }
+  static async updateItem(req, res, next) {
+    try {
+      const { UserId, amountBid } = req.body;
+      const { id } = req.params;
+      const { data } = await axios.put(`${itemAPI}/${id}`, {
+        UserId,
+        amountBid,
+      });
+      invalidateCache(itemCache.items);
+      res.status(200).json(data);
+    } catch (err) {
+      next(err);
     }
-
+  }
   static async getReporting(req, res, next) {
     try {
       const { data: report } = await axios.get(mongoAPI + "/reporting");
@@ -171,6 +180,10 @@ module.exports = class ItemController {
           itemName,
           reason,
         }
+      );
+      res.status(201).json(postReporting);
+    } catch (error) {
+      next(error);
     }
   }
   static async deleteReporting(req, res, next) {
@@ -200,31 +213,39 @@ module.exports = class ItemController {
           chatValue,
           isSeller,
         }
+      );
+      res.status(201).json(upPost);
+    } catch (error) {
+      next(error);
     }
-    static async postBid(req, res, next) {
-        const { UserId, username, bidValue } = req.body;
-        const { id } = req.params;
-        try {
-            const { data: dataBid } = await axios.post(mongoAPI + `/itemHistory/${id}/bid`, {
-                UserId,
-                username,
-                bidValue,
-            });
-            res.status(201).json(dataBid);
-        } catch (error) {
-            next(error);
+  }
+  static async postBid(req, res, next) {
+    const { UserId, username, bidValue } = req.body;
+    const { id } = req.params;
+    try {
+      const { data: dataBid } = await axios.post(
+        mongoAPI + `/itemHistory/${id}/bid`,
+        {
+          UserId,
+          username,
+          bidValue,
         }
+      );
+      res.status(201).json(dataBid);
+    } catch (error) {
+      next(error);
     }
-    static async itemHistory(req, res, next) {
-        try {
-            const { id } = req.params;
-            const { data: historyItems } = await axios.get(mongoAPI + `/itemHistory/${id}`);
-            res.status(200).json(historyItems);
-        } catch (error) {
-            next(error);
-        }
+  }
+  static async itemHistory(req, res, next) {
+    try {
+      const { id } = req.params;
+      const { data: historyItems } = await axios.get(
+        mongoAPI + `/itemHistory/${id}`
+      );
+      res.status(200).json(historyItems);
+    } catch (error) {
+      next(error);
     }
-
   }
   static async myAuction(req, res, next) {
     try {
@@ -237,5 +258,4 @@ module.exports = class ItemController {
       next(error);
     }
   }
-
 };
