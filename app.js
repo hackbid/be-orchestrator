7dba97f710bf7998cd4a516e1e29b738a6bfcd2f
mashayyik <mashayyik@gmail.com>
2023-03-08 11:24:30 +0700

    feat: integrated socket.io

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ const io = require('socket.io')(server);
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const port = process.env.PORT || 4000;
-
+const axios = require('axios');
 //import routes
 const router = require('./routes/index');
 const errorHandler = require('./middlewares/errorHandler');
 
-//set up middleware
+//set ENV
+const itemAPI = process.env.ITEM_API;
+const mongoAPI = process.env.MONGO_API;
+const userAPI = process.env.USER_API;
+const categoryAPI = process.env.CATEGORY_API;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -21,8 +25,36 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router);
 app.use(errorHandler);
 
+//set up socket.io
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('joinRoom', (payload) => {
+        const { roomId, username, UserId } = payload;
+        socket.join(roomId);
+        const message = `${username} has joined the room`;
+        console.log(message);
+        io.to(roomId).emit('connectedToRoom', { message });
+    });
+    socket.on('bid', async (payload) => {
+        try {
+            const { roomId, username, UserId, bidValue, historyMongoId } = payload;
+            await axios.put(`${itemAPI}/${roomId}`, { UserId, amountBid: bidValue });
+            io.to(roomId).emit('bidSuccess', { username, amountBid: bidValue });
+            // await axios.post(`${mongoAPI}/itemHistory/${historyMongoId}/bid`, { UserId, username, bidValue });//test
+        } catch (err) {
+            socket.emit('bidFailed', { message: err.response.data.message });
+        }
+    });
+    socket.on('sendMessage', async (payload) => {
+        const { roomId, username, chatValue, isSeller, historyMongoId } = payload;
+        io.to(roomId).emit('messageSuccess', { username, chatValue, isSeller });
+        await axios.post(`${mongoAPI}/itemHistory/${historyMongoId}/chat`, { username, chatValue, isSeller });
+    });
+});
+
 server.listen(port, function () {
     console.log('Server started on port ' + port);
 });
-
-module.exports = io;
--- controllers/app-service/item/itemControllers.js
+++ controllers/app-service/item/itemControllers.js
@@ -1,221 +1,189 @@
-const { Upload } = require("@aws-sdk/lib-storage");
-const axios = require("axios");
-const redis = require("../../../config/redis.config");
-const { s3 } = require("../../../config/S3");
-const { invalidateCache } = require("../../../helper/invalidate.redis");
-const itemAPI = process.env.itemAPI;
+const { Upload } = require('@aws-sdk/lib-storage');
+const axios = require('axios');
+const redis = require('../../../config/redis.config');
+const { s3 } = require('../../../config/S3');
+const { invalidateCache } = require('../../../helper/invalidate.redis');
+const itemAPI = process.env.ITEM_API;
 const mongoAPI = process.env.MONGO_API;
 const userAPI = process.env.USER_API;
 const categoryAPI = process.env.CATEGORY_API;
 const itemCache = {
-  items: "items/all",
+    items: 'items/all',
 };
 
 module.exports = class ItemController {
-  static async findAll(req, res, next) {
-    try {
-      const cachedData = await redis.get(itemCache.items);
-      if (cachedData) {
-        return res.status(200).json(JSON.parse(cachedData));
-      } else {
-        const { data } = await axios.get(itemAPI);
-        await redis.set(itemCache.items, JSON.stringify(data));
-        res.status(200).json(data);
-      }
-    } catch (err) {
-      next(err);
+    static async findAll(req, res, next) {
+        try {
+            const cachedData = await redis.get(itemCache.items);
+            if (cachedData) {
+                return res.status(200).json(JSON.parse(cachedData));
+            } else {
+                const { data } = await axios.get(itemAPI);
+                await redis.set(itemCache.items, JSON.stringify(data));
+                res.status(200).json(data);
+            }
+        } catch (err) {
+            next(err);
+        }
     }
-  }
-  static async findItemToday(req, res, next) {
-    try {
-      const { data: itemToday } = await axios.get(`${itemAPI}/today`);
-      const temp = itemToday.map(async (e) => {
-        const { data: imagesData } = await axios.get(
-          mongoAPI + `/itemImages/${e.imageMongoId}`
-        );
-        e.images = imagesData ? imagesData.images : [];
-        return e;
-      });
-      const result = await Promise.all(temp);
-      res.status(200).json(result);
-    } catch (err) {
-      next(err);
+    static async findItemToday(req, res, next) {
+        try {
+            const { data: itemToday } = await axios.get(`${itemAPI}/today`);
+            const temp = itemToday.map(async (e) => {
+                const { data: imagesData } = await axios.get(mongoAPI + `/itemImages/${e.imageMongoId}`);
+                e.images = imagesData ? imagesData.images : [];
+                return e;
+            });
+            const result = await Promise.all(temp);
+            res.status(200).json(result);
+        } catch (err) {
+            next(err);
+        }
     }
-  }
-  static async createItem(req, res, next) {
-    try {
-      const {
-        name,
-        SubCategoryId,
-        description,
-        startPrice,
-        multiple,
-        startDate,
-        startHour,
-        endHour,
-        weight,
-        UserId,
-      } = req.body;
-      const { data } = await axios.post(itemAPI, {
-        name,
-        SubCategoryId,
-        description,
-        startPrice,
-        multiple,
-        startDate,
-        startHour,
-        endHour,
-        status: "posted",
-        weight,
-        UserId,
-      });
-      const imagesUrl = req.files.map(async (file) => {
-        const params = {
-          Bucket: "data-hackbid-app",
-          Key: Math.floor(Math.random() * 1000) + "_" + `${name}_assets`,
-          Body: file.buffer,
-        };
-        const uploadParallel = new Upload({
-          client: s3,
-          queueSize: 4, // optional concurrency configuration
-          partSize: 5542880, // optional size of each part
-          leavePartsOnError: false, // optional manually handle dropped parts
-          params,
-        });
-        const { Location } = await uploadParallel.done();
-        return Location;
-      });
-      const images = await Promise.all(imagesUrl);
-      await axios.post(`${mongoAPI}/itemImages/${data.imageMongoId}`, {
-        images,
-      });
-      invalidateCache(itemCache.items);
-      res.status(200).json(data);
-    } catch (err) {
-      // next(err);
-      console.log(err);
+    static async createItem(req, res, next) {
+        try {
+            const { name, SubCategoryId, description, startPrice, multiple, startDate, startHour, endHour, weight, UserId } = req.body;
+            const { data } = await axios.post(itemAPI, {
+                name,
+                SubCategoryId,
+                description,
+                startPrice,
+                multiple,
+                startDate,
+                startHour,
+                endHour,
+                status: 'posted',
+                weight,
+                UserId,
+            });
+            const imagesUrl = req.files.map(async (file) => {
+                const params = {
+                    Bucket: 'data-hackbid-app',
+                    Key: Math.floor(Math.random() * 1000) + '_' + `${name}_assets`,
+                    Body: file.buffer,
+                };
+                const uploadParallel = new Upload({
+                    client: s3,
+                    queueSize: 4, // optional concurrency configuration
+                    partSize: 5542880, // optional size of each part
+                    leavePartsOnError: false, // optional manually handle dropped parts
+                    params,
+                });
+                const { Location } = await uploadParallel.done();
+                return Location;
+            });
+            const images = await Promise.all(imagesUrl);
+            await axios.post(`${mongoAPI}/itemImages/${data.imageMongoId}`, {
+                images,
+            });
+            invalidateCache(itemCache.items);
+            res.status(200).json(data);
+        } catch (err) {
+            // next(err);
+            console.log(err);
+        }
     }
-  }
 
-  static async getItemById(req, res, next) {
-    try {
-      const { id } = req.params;
-      const { data: itemId } = await axios(`${itemAPI}/${id}`);
-      const { data: imagesData } = await axios.get(
-        mongoAPI + `/itemImages/${itemId.imageMongoId}`
-      );
-      const { data: historyData } = await axios.get(
-        mongoAPI + `/itemHistory/${itemId.historyMongoId}`
-      );
-      const { data: UserId } = await axios.get(
-        userAPI + `/users/${itemId.UserId}`
-      );
-      let user = itemId.Winner.UserId;
-      const { data: winnerId } = await axios.get(userAPI + `/users/${user}`);
-      itemId.Winner.username = winnerId.username;
-      itemId.images = imagesData ? imagesData.images : [];
-      itemId.chats = historyData ? historyData.chatHistories : [];
-      itemId.bids = historyData ? historyData.bidHistories : [];
-      itemId.seller = UserId ? UserId : "";
-      res.status(200).json(itemId);
-    } catch (err) {
-      console.log(err);
-      next(err);
+    static async getItemById(req, res, next) {
+        try {
+            const { id } = req.params;
+            const { data: itemId } = await axios(`${itemAPI}/${id}`);
+            const { data: imagesData } = await axios.get(mongoAPI + `/itemImages/${itemId.imageMongoId}`);
+            const { data: historyData } = await axios.get(mongoAPI + `/itemHistory/${itemId.historyMongoId}`);
+            const { data: UserId } = await axios.get(userAPI + `/users/${itemId.UserId}`);
+            let user = itemId.Winner.UserId;
+            const { data: winnerId } = await axios.get(userAPI + `/users/${user}`);
+            itemId.Winner.username = winnerId.username;
+            itemId.images = imagesData ? imagesData.images : [];
+            itemId.chats = historyData ? historyData.chatHistories : [];
+            itemId.bids = historyData ? historyData.bidHistories : [];
+            itemId.seller = UserId ? UserId : '';
+            res.status(200).json(itemId);
+        } catch (err) {
+            console.log(err);
+            next(err);
+        }
     }
-  }
-  static async deleteItem(req, res, next) {
-    try {
-      const { id } = req.params;
-      const { data } = await axios.delete(`${itemAPI}/${id}`);
-      invalidateCache(itemCache.items);
-      res.status(200).json(data);
-      res.status(200).json(data);
-    } catch (err) {
-      next(err);
+    static async deleteItem(req, res, next) {
+        try {
+            const { id } = req.params;
+            const { data } = await axios.delete(`${itemAPI}/${id}`);
+            invalidateCache(itemCache.items);
+            res.status(200).json(data);
+            res.status(200).json(data);
+        } catch (err) {
+            next(err);
+        }
     }
-  }
 
-  static async updateItem(req, res, next) {
-    try {
-      const { UserId, amountBid } = req.body;
-      const { id } = req.params;
-      const { data } = await axios.put(`${itemAPI}/${id}`, {
-        UserId,
-        amountBid,
-      });
-      invalidateCache(itemCache.items);
-      res.status(200).json(data);
-    } catch (err) {
-      next(err);
-    }
-  }
-  static async getReporting(req, res, next) {
-    const { data: report } = await axios
-      .get(mongoAPI + "/reporting")
-      .catch(next);
-    res.json(report);
-  }
-  static async postReporting(req, res, next) {
-    try {
-      const { UserId, username, itemId, itemName, reason } = req.body;
-      const { data: postReporting } = await axios.post(
-        mongoAPI + "/reporting",
-        {
-          UserId,
-          username,
-          itemId,
-          itemName,
-          reason,
+    static async updateItem(req, res, next) {
+        try {
+            const { UserId, amountBid } = req.body;
+            const { id } = req.params;
+            const { data } = await axios.put(`${itemAPI}/${id}`, {
+                UserId,
+                amountBid,
+            });
+            invalidateCache(itemCache.items);
+            res.status(200).json(data);
+        } catch (err) {
+            next(err);
         }
-      );
-      res.status(201).json(postReporting);
-    } catch (error) {
-      next(error);
     }
-  }
-  static async postChat(req, res, next) {
-    try {
-      const { id } = req.params;
-      const { username, chatValue, isSeller } = req.body;
-      const { data: upPost } = await axios.post(
-        mongoAPI + `/itemHistory/${id}/chat`,
-        {
-          username,
-          chatValue,
-          isSeller,
+    static async getReporting(req, res, next) {
+        const { data: report } = await axios.get(mongoAPI + '/reporting').catch(next);
+        res.json(report);
+    }
+    static async postReporting(req, res, next) {
+        try {
+            const { UserId, username, itemId, itemName, reason } = req.body;
+            const { data: postReporting } = await axios.post(mongoAPI + '/reporting', {
+                UserId,
+                username,
+                itemId,
+                itemName,
+                reason,
+            });
+            res.status(201).json(postReporting);
+        } catch (error) {
+            next(error);
         }
-      );
-      res.status(201).json(upPost);
-    } catch (error) {
-      next(error);
     }
-  }
-  static async postBid(req, res, next) {
-    const { UserId, username, bidValue } = req.body;
-    const { id } = req.params;
-    try {
-      const { data: dataBid } = await axios.post(
-        mongoAPI + `/itemHistory/${id}/bid`,
-        {
-          UserId,
-          username,
-          bidValue,
+    static async postChat(req, res, next) {
+        try {
+            const { id } = req.params;
+            const { username, chatValue, isSeller } = req.body;
+            const { data: upPost } = await axios.post(mongoAPI + `/itemHistory/${id}/chat`, {
+                username,
+                chatValue,
+                isSeller,
+            });
+            res.status(201).json(upPost);
+        } catch (error) {
+            next(error);
         }
-      );
-      res.status(201).json(dataBid);
-    } catch (error) {
-      next(error);
     }
-  }
-  static async itemHistory(req, res, next) {
-    try {
-      const { id } = req.params;
-      const { data: historyItems } = await axios.get(
-        mongoAPI + `/itemHistory/${id}`
-      );
-      res.status(200).json(historyItems);
-    } catch (error) {
-      next(error);
+    static async postBid(req, res, next) {
+        const { UserId, username, bidValue } = req.body;
+        const { id } = req.params;
+        try {
+            const { data: dataBid } = await axios.post(mongoAPI + `/itemHistory/${id}/bid`, {
+                UserId,
+                username,
+                bidValue,
+            });
+            res.status(201).json(dataBid);
+        } catch (error) {
+            next(error);
+        }
     }
-  }
+    static async itemHistory(req, res, next) {
+        try {
+            const { id } = req.params;
+            const { data: historyItems } = await axios.get(mongoAPI + `/itemHistory/${id}`);
+            res.status(200).json(historyItems);
+        } catch (error) {
+            next(error);
+        }
+    }
 };
